4ca95f53f2ed6738161917839f4a4dc05f4ca90d
chrox <chrox.huang@gmail.com>
2016-08-15 15:21:43 +0800

    add tests for OCR

--- .ci/script.sh
+++ .ci/script.sh
@@ -5,7 +5,18 @@ source "${CI_DIR}/common.sh"
 
 travis_retry make fetchthirdparty
 make all
+
+# install tesseract trained language data for testing OCR functionality
+travis_retry wget https://tesseract-ocr.googlecode.com/files/tesseract-ocr-3.02.eng.tar.gz
+tar zxf tesseract-ocr-3.02.eng.tar.gz
+
+pushd koreader-emulator*/koreader
+    export TESSDATA_PREFIX=`pwd`/data
+    mv ../../tesseract-ocr/tessdata data/
+popd
+
 make testfront
+
 set +o pipefail
 luajit $(which luacheck) --no-color -q frontend | tee ./luacheck.out
 test $(grep Total ./luacheck.out | awk '{print $2}') -le 19
